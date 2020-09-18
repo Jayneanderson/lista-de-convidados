cca61a4c520e72121810d66a0ac4d2b50dfac5b1
Jayneanderson <jayneanderson.santos@gmail.com>
2020-09-18 14:03:20 -0300

    criando projeto, baixando dependências e configurando banco

---
+++ src/main/java/com/style3/evento/GestaoEventoApplication.java
@@ -0,0 +1,13 @@
+package com.style3.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEventoApplication.class, args);
+	}
+
+}
